bd82cec24c6dcd02948b72cbc0c59d1023b6f76c
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2026-04-09 07:59:54 +0900

    map画面を遅延ロード

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,24 @@
 import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App.tsx";
+import LandingPage from "./pages/LandingPage";
 
-const LandingPage = lazy(() => import("./pages/LandingPage"));
+const App = lazy(() => import("./App"));
 const EduApp = lazy(() => import("./edu/EduApp"));
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route
-          path="/"
+          path="/pro"
           element={
-            <Suspense fallback={<div />}>
-              <LandingPage />
+            <Suspense fallback={<div>Loading...</div>}>
+              <App />
             </Suspense>
           }
         />
-        <Route path="/pro" element={<App />} />
         <Route
           path="/edu/*"
           element={
